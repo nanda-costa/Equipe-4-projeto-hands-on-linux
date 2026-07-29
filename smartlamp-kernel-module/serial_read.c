a6b7a22cbc5fe6e229fe886d56306d33a1c92add
Luizzbar <luiz.costa@icomp.ufam.edu.br>
2026-07-28 20:58:52 -0400

    pequenas alteracoes no serial_read.c

--- smartlamp-kernel-module/serial_read.c
+++ smartlamp-kernel-module/serial_read.c
@@ -1,5 +1,5 @@
 #include <linux/module.h>
-#include <string.h>
+#include <linux/string.h>
 #include <linux/usb.h>
 #include <linux/slab.h>
 
@@ -16,8 +16,8 @@ static uint usb_in, usb_out;                       // Endereços das portas de e
 static char *usb_in_buffer, *usb_out_buffer;       // Buffers de entrada e saída da USB
 static int usb_max_size;                           // Tamanho máximo de uma mensagem USB
 
-#define VENDOR_ID   SUBSTITUA_PELO_VENDORID /* Encontre o VendorID  do smartlamp */
-#define PRODUCT_ID  SUBSTITUA_PELO_PRODUCTID /* Encontre o ProductID do smartlamp */
+#define VENDOR_ID  0x10c4/* Encontre o VendorID  do smartlamp */
+#define PRODUCT_ID 0xea60  /* Encontre o ProductID do smartlamp */
 static const struct usb_device_id id_table[] = { { USB_DEVICE(VENDOR_ID, PRODUCT_ID) }, {} };
 
 static int  usb_probe(struct usb_interface *ifce, const struct usb_device_id *id); // Executado quando o dispositivo é conectado na USB
@@ -195,7 +195,7 @@ static int usb_read_serial(void) {
     char *termo = strrchr(recv_line, ' ');
 
     if (termo){ // Encontra o valor numerico da string, o numero estara depois dele.
-        ret = kstrol(termo + 1, 10, &val);
+        ret = kstrtol(termo + 1, 10, &val);
         if(ret == 0){
             return val;
         }else{
